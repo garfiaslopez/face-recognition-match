d9573bba35123f985e85c319dfb6f81aee61c230
Jose Garfias <garfiaslopez@hotmail.com>
2021-12-24 10:52:16 -0600

    add ios project

--- augment.py
+++ augment.py
@@ -7,7 +7,7 @@ def data_processing(root_dir: str):
   data.zoom(probability=0.5, min_factor=1.1, max_factor=1.5)
   data.skew(probability=0.5, magnitude=0.5)
   data.shear(probability=0.5, max_shear_left=10, max_shear_right=10)
-  data.crop_random(probability=0.5, percentage_area=0.9, randomise_percentage_area=True)
-  data.sample(1500)
+  # data.crop_random(probability=0.5, percentage_area=0.9, randomise_percentage_area=True)
+  data.sample(500)
 
-data_processing('Training Data')
+data_processing('photos')
